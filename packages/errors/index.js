458bb32adf470d3ea2f43cfb6cc866a6b7b5127e
santiago <santiagotaschetta@gmail.com>
2021-11-13 22:25:12 -0300

    added @taschetta/errors package

---
+++ packages/errors/index.js
@@ -0,0 +1,40 @@
+
+export class MissingDataError extends Error {
+  constructor() {
+    super(`Faltan datos`)
+    this.name = 'MissingDataError'
+    if(Error.captureStackTrace) {
+      Error.captureStackTrace(this, MissingDataError)
+    }
+  }
+}
+
+export class InvalidUsernameError extends Error {
+  constructor() {
+    super(`No se encontró el usuario`)
+    this.name = 'InvalidUsernameError'
+    if(Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvalidUsernameError)
+    }
+  }
+}
+
+export class InvalidPasswordError extends Error {
+  constructor() {
+    super(`Contraseña invalida`)
+    this.name = 'InvalidPasswordError'
+    if(Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvalidPasswordError)
+    }
+  }
+}
+
+export class InvalidTokenError extends Error {
+  constructor() {
+    super('El token provisto es invalido')
+    this.name = 'InvalidTokenError'
+    if(Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvalidTokenError)
+    }
+  }
+}
