8866bda330324d0dcd4b0ec0e7337af07f2c5e24
santiago <santiagotaschetta@gmail.com>
2021-11-20 18:59:35 -0300

    refactor table.test.js on the table package

--- packages/table/test/table.test.js
+++ packages/table/test/table.test.js
@@ -5,30 +5,38 @@ import { format } from 'mysql2'
 import useBuilder from '../src/builder.js'
 import useTable from '../src/table.js'
 
-const rows = [
-  { id: 1, active: true,  code: 'AAA',   name: 'Amsdn Uniasd', age: 22 },
-  { id: 2, active: true,  code: 'ABA',   name: 'Uoane Posos', age: 54 },
-  { id: 3, active: false, code: 'ABABA', name: 'Aobeu Ufhul', age: 47 },
-]
-
-const fields = []
-
-const name = 'test'
 
 describe('the useTable(dependencies)({ name }) module', () => {
-
-  let connection
-  let builder
+  
+  // Data
+  let rows, fields, name
+  // Modules
+  let connection, builder
+  // The table module
   let table
 
   beforeEach(() => {
+    // Data
+    rows = [
+      { id: 1, active: true,  code: 'AAA',   name: 'Amsdn Uniasd', age: 22 },
+      { id: 2, active: true,  code: 'ABA',   name: 'Uoane Posos', age: 54 },
+      { id: 3, active: false, code: 'ABABA', name: 'Aobeu Ufhul', age: 47 },
+    ]  
+
+    fields = []
+    
+    name = 'test'
+    
+    // Modules    
     connection = {
       query: mock(() => [rows, fields]) 
     }
-
-    builder = useBuilder({ format })
     
+    builder = useBuilder({ format })
+
+    // The table module    
     table = useTable({ connection, builder })(name)
+    
   })
   
   describe('when the findMany(query, options) function is called', () => {
@@ -163,6 +171,15 @@ describe('the useTable(dependencies)({ name }) module', () => {
       method: 'findOne',
       before: 'SELECT * FROM `test`',
       after: 'LIMIT 1'
+    })
+    
+  })
+
+  describe('when the removeMany(query, options) function is called', () => {
+
+    it('removes all rows from the table', async () => {
+      await table.removeMany()
+      checkQuery('DELETE FROM `test`')
     })
     
   })
