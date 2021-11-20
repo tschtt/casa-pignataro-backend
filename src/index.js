1e4f090e1df95b177c891510a75319a8dcbd2cb8
santiago <santiagotaschetta@gmail.com>
2021-11-20 13:37:53 -0300

    translated message on src/index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import app from './_app.js'
 const PORT = process.env.APP_PORT
 
 app.listen(PORT, () => {
-  console.log(`Escuchando en puerto ${PORT}`)
+  console.log(`Listening on port ${PORT}`)
 })
