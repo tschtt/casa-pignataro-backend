2f67e068f1056b102e50ce50ddb869f01900632e
santiago <santiagotaschetta@gmail.com>
2021-11-20 13:19:02 -0300

    imported admin from the session module (src/session)

--- src/session/index.js
+++ src/session/index.js
@@ -1,6 +1,8 @@
 import auth from '@packages/auth'
 import hash from '@packages/hash'
 
+import { controller as admins } from '../admins/index.js'
+
 import useEndpoint from './_endpoint.js'
 import useRouter from './_router.js'
 
