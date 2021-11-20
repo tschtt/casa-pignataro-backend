a11820239fe02eab0e30076882acd3bcce86b768
santiago <santiagotaschetta@gmail.com>
2021-11-20 13:16:24 -0300

    renamed @packages/database to @packages/table

--- packages/table/src/index.js
+++ packages/table/src/index.js
@@ -6,7 +6,7 @@ const database = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '7Ujm6Yhn2Wsx',
-  database: 'pecomtos',
+  database: 'casa_pignataro',
 })
 
 const builder = useBuilder({ format })
