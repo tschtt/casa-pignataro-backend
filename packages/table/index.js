90336e2428686dee777643f309131b4cba973dc9
santiago <santiagotaschetta@gmail.com>
2021-11-21 20:59:02 -0300

    deleted calls to connection on @packages/table

--- packages/table/index.js
+++ packages/table/index.js
@@ -6,10 +6,5 @@ import configTable from './src/table.js'
 export const connection = await useConnection({ mysql })
 export const builder = useBuilder({ format })
 export const useTable = configTable({ connection, builder })
+
 export default useTable
-
-const result = await connection.query("UPDATE `admin_session` SET `fkAdmin` = 2 WHERE `id` = 3")
-
-console.log(result)
-
-connection.end()
