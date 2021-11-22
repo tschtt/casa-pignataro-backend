d7df87f8aef401a9d2e98eef9280326a45b7d546
santiago <santiagotaschetta@gmail.com>
2021-11-22 16:42:24 -0300

    added PORT var to database connection

--- packages/table/src/connection.js
+++ packages/table/src/connection.js
@@ -3,6 +3,7 @@ const HOST = process.env.DATABASE_HOST
 const USER = process.env.DATABASE_USER
 const PASSWORD = process.env.DATABASE_PASSWORD
 const NAME = process.env.DATABASE_NAME
+const PORT = process.env.DATABASE_PORT
 
 export default async ({ mysql }) => {
   const connection = await mysql.createConnection({
@@ -10,6 +11,7 @@ export default async ({ mysql }) => {
     user: USER,
     password: PASSWORD,
     database: NAME,
+    port: PORT
   })
 
   console.log('Connected to the database')
