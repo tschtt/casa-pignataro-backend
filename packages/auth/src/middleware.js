e0ecaaac1fee19be224bfa4b4df1653d2d541501
santiago <santiagotaschetta@gmail.com>
2021-11-22 12:46:01 -0300

    added @packages/auth middleware

---
+++ packages/auth/src/middleware.js
@@ -0,0 +1,36 @@
+import { AuthenticationFailedError } from "../src/errors.js"
+
+
+export default ({ auth }) => (req, res, next) => {
+  const authorization = req.headers.authorization
+
+  if(!authorization) {
+    throw new AuthenticationFailedError('el header authorization no esta definido')
+  }
+  
+  const prefix = authorization.split(' ')[0] 
+  const token = authorization.split(' ')[1]
+
+  if(prefix.toLowerCase() !== 'bearer') {
+    throw new AuthenticationFailedError('el encabezado de autorizacion no cumple con el formato requerido')
+  }
+
+  let payload
+  
+  try {
+    payload = auth.decode(token)    
+  } catch {
+    throw new AuthenticationFailedError('el token provisto no es valido')
+  }
+
+  const PAYLOAD_TYPE = process.env.AUTH_PAYLOAD_TYPE
+
+  if(PAYLOAD_TYPE && payload.type !== PAYLOAD_TYPE) {
+    throw new AuthenticationFailedError('el token provisto no es valido')
+  }
+  
+  req.auth = {}
+  req.auth.payload = payload
+
+  next()
+}
---
+++ packages/auth/test/middleware.test.js
@@ -0,0 +1,97 @@
+import { expect } from "chai"
+import mock from "./helpers/mock.js"
+
+import { AuthenticationFailedError } from '../src/errors.js'
+import useAdmin from "../src/middleware.js"
+
+describe("the admin({ auth })(req, res, next) middleware", () => {
+
+  // Data
+  let token
+  let payload
+  let req, res, next
+  // Modules
+  let auth
+  // The admin middleware
+  let admin
+  
+  beforeEach(() => {
+    // Data
+    token = 'BASE.TOKEN.EXAMPLE'
+    payload = { id: 5, type: 'access' }
+    req = {
+      headers: {
+        authorization: `bearer ${token}`
+      }
+    }
+    res = {}
+    next = mock()
+    // Modules
+    auth = {
+      decode: mock(() => payload)
+    }
+    // Admin middleware
+    admin = useAdmin({ auth })
+  })
+
+  it("decodes the token part from req.headers.authorization", () => {
+    admin(req, res, next)
+    expect(auth.decode.mock.calls[0][0]).to.equals(token)
+  })
+
+  it("adds the token's payload to req.auth.payload", () => {
+    admin(req, res, next)
+    expect(req.auth.payload).to.equals(payload)
+  })
+
+  it("calls the next function", () => {
+    admin(req, res, next)
+    expect(next.mock.calls.length).to.equals(1)
+  })
+
+  describe("if req.headers.authorization is undefined", () => {
+    
+    it("throws an AuthenticationFailedError with a message like 'No se pudo autenticar su pedido: el header authorization no esta definido'", () => {
+      delete req.headers.authorization
+      expect(() => admin(req, res, next)).to.throw(AuthenticationFailedError)
+      expect(() => admin(req, res, next)).to.throw('No se pudo autenticar su pedido: el header authorization no esta definido')
+    })
+    
+  })
+
+  describe("if req.headers.authorization does not have an 'bearer ${token}' format", () => {
+    
+    it("throws an AuthenticationFailedError with a message like 'No se pudo autenticar su pedido: el encabezado de autorizacion no cumple con el formato requerido'", () => {
+      req.headers.authorization = token
+      expect(() => admin(req, res, next)).to.throw(AuthenticationFailedError)
+      expect(() => admin(req, res, next)).to.throw('No se pudo autenticar su pedido: el encabezado de autorizacion no cumple con el formato requerido')
+    })
+    
+  })
+
+  describe("if the token is not valid", () => {
+
+    it("throws an AuthenticationFailedError with a message like 'No se pudo autenticar su pedido: el token provisto no es valido'", () => {
+      auth.decode = mock(() => { throw new Error()})
+      expect(() => admin(req, res, next)).to.throw(AuthenticationFailedError)
+      expect(() => admin(req, res, next)).to.throw('No se pudo autenticar su pedido: el token provisto no es valido')
+    })
+    
+  })
+
+  describe("if an AUTH_PAYLOAD_TYPE enviroment variable is set", () => {
+    
+    describe("and if the token's type is not equal to it", () => {
+      
+      it("throws an AuthenticationFailedError with a message like 'No se pudo autenticar su pedido: el token provisto no es valido'", () => {
+        process.env.AUTH_PAYLOAD_TYPE = 'access'
+        payload.type = 'session'
+        expect(() => admin(req, res, next)).to.throw(AuthenticationFailedError)
+        expect(() => admin(req, res, next)).to.throw('No se pudo autenticar su pedido: el token provisto no es valido')
+      })
+    })
+
+  })
+
+  
+})
