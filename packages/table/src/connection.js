e9f3558cb3bbc34d91f6eb054d539c28502b78bf
santiago <santiagotaschetta@gmail.com>
2021-12-06 20:37:36 -0300

    changed database connection to use pools

--- packages/table/src/connection.js
+++ packages/table/src/connection.js
@@ -7,12 +7,15 @@ const NAME = process.env.DATABASE_NAME
 const PORT = process.env.DATABASE_PORT
 
 export default async ({ mysql }) => {
-  const connection = await mysql.createConnection({
+  const connection = await mysql.createPool({
     host: HOST,
     user: USER,
     password: PASSWORD,
     database: NAME,
     port: PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
   })
 
   console.log('Connected to the database')
