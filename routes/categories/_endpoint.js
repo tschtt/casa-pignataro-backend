2ba6ddd66bb5ca43c0f2dbaf78eef614864a53ab
santiago <santiagotaschetta@gmail.com>
2021-12-26 17:12:25 -0300

    changed categories routes to start using the categories resource

--- routes/categories/_endpoint.js
+++ routes/categories/_endpoint.js
@@ -1,75 +1,40 @@
 /* eslint-disable default-param-last */
 
-export default ({ table }) => ({
+function parseBoolean(value) {
+  if (value === 'true') return true
+  if (value === 'false') return false
+  return false
+}
+
+export default ({ table, $categories }) => ({
 
   async findMany(request) {
-    const items = await table.findMany()
-
-    if (request.query.flat !== undefined) {
-      const format = (items = [], fkCategorie = 0, path) => {
-        const levelItems = items.filter((item) => item.fkCategorie === fkCategorie)
-        levelItems.forEach((item) => {
-          // const index = items.findIndex((i) => i.id === item.id)
-
-          item.full = path
-            ? [path, item.name].join(' / ')
-            : item.name
-
-          item.categories = format(items, item.id, item.full)
-
-          delete item.fkCategorie
-
-          // items[index] = { ...item }
-        })
-      }
-      format(items)
-
-      return items
-    }
-
-    const format = (items = [], fkCategorie = 0, path) => {
-      const levelItems = items.filter((item) => item.fkCategorie === fkCategorie)
-      return levelItems.map((item) => {
-        item.full = path
-          ? [path, item.name].join(' / ')
-          : item.name
-
-        item.categories = format(items, item.id, item.full)
-
-        delete item.fkCategorie
-
-        return item
-      })
-    }
-
-    return format(items)
+    const flat = parseBoolean(request.query.flat)
+    const items = await $categories.findMany({}, { flat })
+    return items
   },
 
   async findOne(request) {
-    const item = await table.findOne({ id: request.params.id })
-    if (item) {
-      item.categories = await table.findMany({ fkCategorie: request.params.id })
-    }
-    return item || {}
+    const id = parseInt(request.params.id)
+    const item = await $categories.findOne({ id })
+    return item
   },
 
   async insertOne(request) {
     const data = request.body
-    delete data.categories
     const id = await table.insertOne(data)
-    return table.findOne({ id })
+    return $categories.findOne({ id })
   },
 
   async updateOne(request) {
     const id = parseInt(request.params.id)
     const data = request.body
-    delete data.categories
-    await table.updateOne({ id }, data)
-    return table.findOne({ id })
+    await $categories.updateOne({ id, ...data })
+    return $categories.findOne({ id })
   },
 
   async removeOne(request) {
-    return table.removeOne({ id: request.params.id })
+    return $categories.removeOne({ id: request.params.id })
   },
 
 })
