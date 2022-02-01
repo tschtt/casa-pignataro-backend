266337b48aca27d4778186af6835ffc4bf10f1d4
santiago <santiagotaschetta@gmail.com>
2022-02-01 19:39:44 -0300

    cambio PATCH por PUT en /articles

--- routes/articles/_router.js
+++ routes/articles/_router.js
@@ -5,7 +5,7 @@ import { useRouter } from '@packages/router'
 export default ({ endpoint }) => useRouter({
   '/:id': {
     get: endpoint.findOne,
-    patch: [ admin, upload.array('images'), endpoint.updateOne ],
+    put: [ admin, upload.array('images'), endpoint.updateOne ],
     delete: [ admin, endpoint.removeOne ],
   },
   '/': {
